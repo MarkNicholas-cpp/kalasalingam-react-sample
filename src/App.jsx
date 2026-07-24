9f046d9bd8e0feb9d4b196ac3198530d5fb24f30
marknicholas-cpp <marknicholas2002112@gmail.com>
2026-07-24 12:23:24 +0530

    counter usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
+import { useState } from 'react';
 export default function App(){
-  let counter = 0;
+  const [counter,setCounter] = useState(0);
   return <>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni illo neque ad ut fugiat assumenda eligendi consectetur aut, sit ex nisi cumque impedit praesentium veniam amet deleniti, voluptatum nostrum quod?
+  Voluptatibus mollitia nostrum optio exercitationem iusto perferendis veniam inventore ab, quas rerum. In deserunt libero, impedit laborum similique suscipit tempore pariatur adipisci, dolorum, culpa ipsum quis. Eum fugit ullam tenetur?</p>
   {/* Events in react */}
     <button onClick={() => { 
-      counter+=1;
+      setCounter(counter + 1);
     }}>+</button>
-    
+
     <button onClick={() => console.log(counter)}>{counter}</button>
 
     <button onClick={() => { 
-      counter-=1;
+      setCounter(counter - 1);
     }}>-</button>
   </>
 }; 
